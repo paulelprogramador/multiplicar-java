1e65f94bdbaa12252189a35bedfd48768eac87d6
elsinnombreprogramador <elprogramadorjuancho@gmail.com>
2024-08-05 10:25:12 -0500

    multiplicacion de numeros

---
+++ MultiplicarNumeros/MultiplicarNumeros.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.multiplicarnumeros;
+import java.util.Scanner;
+/**
+ *
+ * @author Usuario
+ */
+public class MultiplicarNumeros {
+
+    public static void main(String[] args) {
+    Scanner lector = new Scanner (System.in);
+    System.out.println("ingrse el primer numero q desea multiplicar");
+    double n = lector.nextDouble();
+    System.out.println("ingrse el segundo numero q desea multiplicar");
+     double n1 = lector.nextDouble();
+    }
+}
